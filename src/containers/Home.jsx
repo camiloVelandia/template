720da68524ec531638e5ab5f5e45d1f7e1172142
Camilo Velandia <velandiacamilo356@gmail.com>
2021-04-15 21:35:54 -0500

    first commit

---
+++ src/containers/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/components/app.css'
+
+const Home = () => {
+  return (
+    <h1>hola mundo</h1>
+  );
+}
+
+export default Home;
---
+++ src/routes/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from '../containers/Home';
+import Layout from '../components/Layout';
+
+const App = () => {
+  return (
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+  );
+}
+
+export default App;
